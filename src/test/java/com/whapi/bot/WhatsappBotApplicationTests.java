bedd8d61cc3e7d3085f45ce6ca14e125fe344965
Bhagawati <bh.ja.699@gmail.com>
2024-11-27 15:49:31 +0530

    update test configurations

--- src/test/java/com/whapi/bot/WhatsappBotApplicationTests.java
+++ src/test/java/com/whapi/bot/WhatsappBotApplicationTests.java
@@ -2,8 +2,11 @@ package com.whapi.bot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-test.properties")
 class WhatsappBotApplicationTests {
     @Test
     void contextLoads() {
